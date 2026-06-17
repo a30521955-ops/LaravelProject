bc0541204ddcfa1595ec4f4112db92e3966cd8fb
ahmed elaraby <a30521955@gmail.com>
2026-06-17 20:56:11 +0300

    Commit 13: Create admin order management and tracking system

---
+++ app/Http/Controllers/Admin/OrderController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Models\Order;
+use Illuminate\Routing\Controller;
+
+class OrderController extends Controller
+{
+    public function index() { }
+    public function show() { }
+    public function updateStatus() { }
+}
