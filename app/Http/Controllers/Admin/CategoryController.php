43464f8de1fe462d1f107f0263f5094b53ca24f0
ahmed elaraby <a30521955@gmail.com>
2026-06-17 20:55:45 +0300

    Commit 11: Build admin category management dashboard

---
+++ app/Http/Controllers/Admin/CategoryController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Models\Category;
+use Illuminate\Routing\Controller;
+
+class CategoryController extends Controller
+{
+    public function index() { }
+    public function create() { }
+    public function store() { }
+    public function edit() { }
+    public function update() { }
+    public function destroy() { }
+}
