2b70f013337b461d3cdddaf3c5672072c8b00a0a
ahmed elaraby <a30521955@gmail.com>
2026-06-17 20:54:59 +0300

    Commit 12: Implement admin product management with image upload

---
+++ app/Http/Controllers/Admin/ProductController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Models\Product;
+use Illuminate\Routing\Controller;
+
+class ProductController extends Controller
+{
+    public function index() { }
+    public function create() { }
+    public function store() { }
+    public function edit() { }
+    public function update() { }
+    public function destroy() { }
+}
